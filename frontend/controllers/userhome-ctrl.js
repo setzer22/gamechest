2cba680c11f4617d29a38507c4de09129685380b
UPCman <hamelegarnett@gmail.com>
2016-07-14 19:41:38 +0200

    ejejejejej

---
+++ frontend/controllers/userhome-ctrl.js
@@ -0,0 +1,10 @@
+var UserHomeCtrl = function($scope, LoginService, ToastService, $state) {
+    $scope.hola = "hola";
+
+
+
+
+};
+
+
+angular.module('GameChestApp').controller('UserHomeCtrl', ['$scope', 'LoginService', 'ToastService', '$state', UserHomeCtrl]);
