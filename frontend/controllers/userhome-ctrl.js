92ef3f4a1f493ba789bf753df55ee9955d0bb917
UPCman <hamelegarnett@gmail.com>
2016-07-15 18:11:02 +0200

    vistaUser

--- frontend/controllers/userhome-ctrl.js
+++ frontend/controllers/userhome-ctrl.js
@@ -1,9 +1,20 @@
 var UserHomeCtrl = function($scope, LoginService, ToastService, $state) {
-    $scope.hola = "hola";
+    //Mapeo nombres scheme a visibles para el usuario
+    $scope.campNames = {"name" : "Nombre",
+                        "year": "Año",
+                        "platform": "Plataformas",
+                        "genre": "Género",
+                        "company": "Compañia",
+                        "developer": "Desarrollador",
+                        "composer": "Compositor"
+                       }
 
-    $scope.show = false;
+   
     
-    $scope.games = ["Final Fantasy VI", "Jumbotron", "Jumbotron Plus"];
+    $scope.show = false; 
+    $scope.games = [{"name": "Final Fantasy VI", "genre": "RPG", "year" : 2010},
+    {"name":"Jumbotron" , "campo_vacio" : ""},
+    {"name":"Jumbotron Plus", "genre": "awesomness"}];
 
 
 };
